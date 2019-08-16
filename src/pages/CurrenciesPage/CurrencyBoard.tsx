b5840af3ac837bfedcd6e934cb77d93b4ab72c26
Rafael Pelle <rafapelle@gmail.com>
2019-08-16 20:42:22 -0300

    Implements a CurrencyBoard component.

--- src/pages/CurrenciesPage/CurrencyBoard.tsx
+++ src/pages/CurrenciesPage/CurrencyBoard.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Currency } from '../../utils/interfaces'
+import { handleMoneyFormat, valueToPercentage } from '../../utils/stringParser'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 
@@ -8,10 +9,21 @@ const CurrencyBoard: React.FC<Props> = (props) => {
 
 	return (
 		<Paper elevation={ 6 } style={ paperStyle }>
-			<Typography>{ name }</Typography>
-			<Typography>{ buy }</Typography>
-			<Typography>{ sell }</Typography>
-			<Typography>{ variation }</Typography>
+			<Typography align='center' color='primary' style={ currencyNameStyle }>
+				{ name }
+			</Typography>
+			<Typography style={ fontContainerStyle }>
+				<span style={ labelStyle }>Compra: </span>
+				<span style={ valueStyle }>{ handleMoneyFormat(buy, 'BRL') }</span>
+			</Typography>
+			<Typography style={ fontContainerStyle }>
+				<span style={ labelStyle }>Venda: </span>
+				<strong style={ valueStyle }>{ handleMoneyFormat(sell, 'BRL') }</strong>
+			</Typography>
+			<Typography style={ fontContainerStyle }>
+				<span style={ labelStyle }>Variação: </span>
+				<strong style={ valueStyle }>{ valueToPercentage(variation) }</strong>
+			</Typography>
 		</Paper>
 	)
 }
@@ -23,6 +35,27 @@ export default CurrencyBoard
 /////////////////////////////////////////////////////////////////
 const paperStyle = {
 	padding: '2em',
+}
+const currencyNameStyle = {
+	fontWeight: 700,
+	fontSize: '1.5em',
+	marginBottom: '1em',
+}
+const fontContainerStyle = {
+	display: 'flex',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+	padding: '0.5em 0',
+}
+const labelStyle = {
+	fontSize: '1.2em',
+	fontWeight: 600,
+	opacity: 0.6,
+}
+const valueStyle = {
+	fontSize: '1.2em',
+	fontWeight: 600,
+	// opacity: 0.6,
 }
 /////////////////////////////////////////////////////////////////
 /////////////////////////// INTERFACES //////////////////////////
--- src/pages/CurrenciesPage/index.tsx
+++ src/pages/CurrenciesPage/index.tsx
@@ -17,8 +17,9 @@ const CurrenciesPage: React.FC<Props> = (props) => {
 	const handleGetCurrencies = async () => {
 		setLoading(true)
 		try {
-			const response = await axios.get(`${FINANCE_URL}?format=json-cors&key=${FINANCE_KEY}`)
-			const currenciesData = response.data.results.currencies
+			// const response = await axios.get(`${FINANCE_URL}?format=json-cors&key=${FINANCE_KEY}`)
+			// const currenciesData = response.data.results.currencies
+			const currenciesData = require('../../utils/hardcodedData.json').results.currencies
 			delete currenciesData.source
 			setCurrencies(currenciesData)
 		} catch (e) {
@@ -27,26 +28,19 @@ const CurrenciesPage: React.FC<Props> = (props) => {
 		setLoading(false)
 	}
 
-	const renderTest = () => {
-		if (currencies) {
-			Object.keys(currencies).forEach((key: string) => <p>{ key }</p>)
-		}
-	}
-
 	return (
-		<div>
-			<Typography variant='h4' color='primary' align='center'>
-				Cotação das moédas
+		<div style={ containerStyle }>
+			<Typography variant='h4' color='primary' align='center' style={ headerStyle }>
+				COTAÇÃO DAS MOEDAS
 			</Typography>
 			{ !loading && (
-				<React.Fragment>
-					{ renderTest() }
+				<div style={ gridStyle }>
 					<CurrencyBoard currency={ currencies.USD } />
 					<CurrencyBoard currency={ currencies.EUR } />
 					<CurrencyBoard currency={ currencies.GBP } />
 					<CurrencyBoard currency={ currencies.ARS } />
 					<CurrencyBoard currency={ currencies.BTC } />
-				</React.Fragment>
+				</div>
 			) }
 		</div>
 	)
@@ -57,7 +51,21 @@ export default CurrenciesPage
 /////////////////////////////////////////////////////////////////
 ///////////////////////////// STYLES ////////////////////////////
 /////////////////////////////////////////////////////////////////
-
+const containerStyle = {
+	maxWidth: '800px',
+	margin: '0 auto',
+	padding: '2em 10px',
+}
+const headerStyle = {
+	fontWeight: 800,
+	letterSpaceing: '5px',
+	marginBottom: '2em',
+}
+const gridStyle = {
+	display: 'grid',
+	gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+	gridGap: '30px',
+}
 /////////////////////////////////////////////////////////////////
 /////////////////////////// INTERFACES //////////////////////////
 /////////////////////////////////////////////////////////////////
