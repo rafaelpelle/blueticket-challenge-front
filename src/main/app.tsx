979fe2daa4c79876282a929bfd8b11f50b3bde7d
Rafael Pelle <rafapelle@gmail.com>
2019-08-17 19:36:42 -0300

    Test API request

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
+import axios from 'axios'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Router, Switch } from 'react-router-dom'
+import { FINANCE_KEY, FINANCE_URL } from '../utils/httpClient'
 import { RootReducerInterface, User } from '../utils/interfaces'
 import { history } from '../router/history'
 import MyRoutes from '../router/myRoutes'
@@ -13,6 +15,7 @@ require('./app.css')
 
 const App: React.FC<Props> = (props) => {
 	const [user, setUser] = React.useState<User | null>(null)
+	const [loading, setLoading] = React.useState(false)
 
 	React.useEffect(() => {
 		// listen to the service-worker registration.onupdatefound event on /src/utils/registerSW.ts.
@@ -22,12 +25,29 @@ const App: React.FC<Props> = (props) => {
 	}, [])
 
 	React.useEffect(() => {
-		setUser(props.user)
+		if (props.user) {
+			setUser(props.user)
+			getFinanceData()
+		}
 	}, [props.user])
 
 	const scrollToTheTop = () => {
 		document.body.scrollTop = 0 // For Safari
 		document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+	}
+
+	const getFinanceData = async () => {
+		setLoading(true)
+		try {
+			// const response = await axios.get(FINANCE_URL)
+			// const response = await axios.get(`${FINANCE_URL}?key=${FINANCE_KEY}`)
+			const response = await axios.get(`${FINANCE_URL}?format=json-cors&key=${FINANCE_KEY}`)
+			console.log(response.data)
+			// setCurrencies(currenciesData)
+		} catch (e) {
+			console.error(e)
+		}
+		setLoading(false)
 	}
 
 	return (
