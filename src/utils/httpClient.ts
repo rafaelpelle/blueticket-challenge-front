1d569503091deeac6b4eded9ae1684a916d20d30
Rafael Pelle <rafapelle@gmail.com>
2019-08-19 09:29:12 -0300

    Changes the FINANCE_KEY because the last one got blocked.

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,2 +1,2 @@
 export const FINANCE_URL = process.env.FINANCE_URL || 'https://api.hgbrasil.com/finance'
-export const FINANCE_KEY = process.env.FINANCE_KEY || 'e503cdf6'
+export const FINANCE_KEY = process.env.FINANCE_KEY || '74f605c5'
